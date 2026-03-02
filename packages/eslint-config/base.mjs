1406289df4a12bb90216c32139180a2785aa8bd2
rczajkadev <rafalcz29@gmail.com>
2026-03-02 18:09:20 +0100

    chore(packages): add eslint-config and tsconfig

---
+++ packages/eslint-config/base.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import onlyWarn from 'eslint-plugin-only-warn';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin
+    },
+    rules: {
+      'turbo/no-undeclared-env-vars': 'warn'
+    }
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn'
+    }
+  },
+  {
+    plugins: {
+      onlyWarn
+    }
+  },
+  {
+    ignores: ['dist/**']
+  }
+];
