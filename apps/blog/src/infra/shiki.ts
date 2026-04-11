4055f003249cd1f0a5d3174bf35b93d881fe49b6
rczajkadev <rafalcz29@gmail.com>
2026-04-11 13:08:25 +0200

    fix(blog): remove shiki eslint warning

--- apps/blog/src/infra/shiki.ts
+++ apps/blog/src/infra/shiki.ts
@@ -6,21 +6,24 @@ const DEFAULT_LANGUAGE = 'text';
 
 type Theme = 'github-dark' | 'github-light';
 
+const stripTabIndex = (html: string) => html.replace(/tabindex="0"/g, '');
+
 export const highlightCode = async (code: string, theme: Theme, language?: string) => {
   const lang = language?.trim() || DEFAULT_LANGUAGE;
-  let html: string = '';
 
   try {
-    html = await codeToHtml(code, {
-      lang,
-      theme
-    });
+    return stripTabIndex(
+      await codeToHtml(code, {
+        lang,
+        theme
+      })
+    );
   } catch {
-    html = await codeToHtml(code, {
-      lang: DEFAULT_LANGUAGE,
-      theme
-    });
+    return stripTabIndex(
+      await codeToHtml(code, {
+        lang: DEFAULT_LANGUAGE,
+        theme
+      })
+    );
   }
-
-  return html.replace(/tabindex="0"/g, '');
 };
